0b42e824bf5dd76bb7b362416f6114d2b1297002
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2022-08-20 18:06:39 +0530

    Add files via upload

---
+++ DepartmentData/SQLDepartmentData.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.DepartmentData
+{
+	public class SQLDepartmentData : IDepartmentData
+	{
+		private Employee_Dept_Context _employeeContext;
+
+		public SQLDepartmentData(Employee_Dept_Context employee_Dept_Context)
+		{
+			_employeeContext = employee_Dept_Context;
+		}
+		public Department AddDepartment(Department department)
+		{
+			department.Department_ID = Guid.NewGuid();
+			_employeeContext.department.Add(department);
+			_employeeContext.SaveChanges();
+			return department;
+		}
+
+		public void DeleteDepartment(Department department)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Department> GetDepartments()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Department ModifyDepartment(Department department)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
