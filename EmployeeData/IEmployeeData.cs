7a8041555bc9e785c109f7bee8f28abb32003d75
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2022-08-20 18:08:06 +0530

    Create IEmployeeData.cs

---
+++ EmployeeData/IEmployeeData.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.EmployeeData
+{
+	public interface IEmployeeData
+	{
+		List<Employee> GetEmployees();
+		Employee GetEmployeeStatus(Guid Employee_ID);
+		Employee AddEmployee(Employee employee);
+		void DeleteEmployee(Employee employee);
+		Employee GetEmployee(Guid id);
+		Employee ModifyEmployee(Employee employee);
+	}
+}
