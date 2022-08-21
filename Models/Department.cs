875633398bedb7dccf3a7abf819a07e1c55c9aa4
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2022-08-21 14:05:37 +0530

    Create Department.cs

---
+++ Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.Models
+{
+	public class Department
+	{
+		[Key]
+		public Guid Department_ID { get; set; }
+
+		public virtual Employee Employee { get; set; }
+		[Required]
+		[Column(TypeName = "Varchar(10)")]
+		[MaxLength(10, ErrorMessage = "department name should not exceed 10 characters")]
+		public string Department_name { get; set; }
+	}
+}
