45d273cb4ae0d505e29f8e22eb31ccc1b4bdee68
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2022-08-21 14:05:51 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.Models
+{
+	public class Employee
+	{
+		
+		[Key]
+		public Guid Employee_ID { get; set; }  // Primary key of employee table
+
+		[Required]
+		[Column(TypeName = "Varchar(10)")]
+		[MaxLength(10, ErrorMessage = "First name should not exceed 10 characters")]
+		public string Firstname { get; set; }
+
+		[Required]
+		[Column(TypeName ="Varchar(10)")]
+		[MaxLength(10, ErrorMessage = "Last name should not exceed 10 characters ")]
+		public string Lastname { get; set; }
+
+		[Required]
+		[RegularExpression(@" ^ ([\w\.\-] +)@([\w\-] +)((\.(\w){2, 3})+)$", ErrorMessage = "Enter a Valid EmailID")]
+		public string EmailId { get; set; }
+
+		[ForeignKey("department")]
+		[Required]
+		public Guid Department_ID { get; set; }
+		public virtual Department department { get; set; }
+
+		[ForeignKey("IdentityUser")]
+		[Required]
+		public int Manager_ID { get; set; }  // Self referencing to primary key of Employee_ID of same table (i.e. Employee table). Foreign key is self referencing
+		//public virtual Employee employee { get; set; }
+		public virtual IdentityUser IdentityUser{ get; set; }
+	}
+}
---
+++ Models/Employee_Dept_Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.Models
+{
+	public class Employee_Dept_Context : DbContext
+	{
+		public Employee_Dept_Context(DbContextOptions<Employee_Dept_Context> options) : base(options)
+		{}
+
+		public DbSet<Employee> employees { get; set; }
+
+		public DbSet<Department> department { get; set; }
+	}
+}
